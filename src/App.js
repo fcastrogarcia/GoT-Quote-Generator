edfb57306a55957496c596c3c683f9920e42158a
fcastrogarcia <fede.castrog@gmail.com>
2019-01-27 18:09:06 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,25 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: "",
+      character: "",
+      isLoaded: false
+    }
+    }
+
+  componentDidMount() {
+    this.fetchData();
+    }
+    fetchData = () => {
+      fetch('https://got-quotes.herokuapp.com/quotes')
+        .then(result => result.json())
+        .then(data => {
+          this.setState({
+            quote: data.quote,
+            character: data.character,
+            isLoaded: true
+          })
+        }
+        )
+    }
+
+
   render() {
-    return (
+    return this.state.isLoaded ? (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <div id="quote-container">
+            <blockquote className="blockquote text-right">
+              <p className="mb-0">{this.state.quote}</p>
+              <footer className="blockquote-footer">{this.state.character}</footer>
+            </blockquote>
+            <button type="button" className="btn btn-primary btn-lg" onClick={this.fetchData}>New Quote</button>
+          </div>
       </div>
-    );
+    ) : (
+      <div className="App">
+      
+      </div>
+    )
   }
 }
 
