1475e4168e7ac4a44c8272d65721f499d1d30734
fcastrogarcia <fede.castrog@gmail.com>
2019-02-12 19:08:41 -0300

    Updates

---
+++ precache-manifest.3b2db58c731548f68c3d09262f2b7011.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GoT-Quote-Generator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e4ea59b2d1aedb078ff8",
+    "url": "/GoT-Quote-Generator/static/js/runtime~main.e4ea59b2.js"
+  },
+  {
+    "revision": "f050f225cd99a3da425f",
+    "url": "/GoT-Quote-Generator/static/js/main.f050f225.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/GoT-Quote-Generator/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "f050f225cd99a3da425f",
+    "url": "/GoT-Quote-Generator/static/css/main.dfdd2b00.chunk.css"
+  },
+  {
+    "revision": "5906cdd36116ce7da4aa9fd50a17c9c2",
+    "url": "/GoT-Quote-Generator/index.html"
+  }
+];
