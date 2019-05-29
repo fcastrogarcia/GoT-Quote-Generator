1a54047bee8ad9604678f91c02b945df7023c166
fcastrogarcia <fede.castrog@gmail.com>
2019-05-28 22:44:46 -0300

    destructuring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
@@ -8,49 +8,57 @@ class App extends Component {
       quote: "",
       character: "",
       isLoaded: false
-    }
+    };
     this.fetchData = this.fetchData.bind(this);
-
-    }
+  }
 
   componentDidMount() {
     this.fetchData();
-    }
+  }
 
   fetchData = () => {
-      fetch('https://got-quotes.herokuapp.com/quotes')
-        .then(result => result.json())
-        .then(data => {
-          this.setState({
-            quote: data.quote,
-            character: data.character,
-            isLoaded: true
-          })
-        }
-      )
-    }
-
+    fetch("https://got-quotes.herokuapp.com/quotes")
+      .then(result => result.json())
+      .then(data => {
+        this.setState({
+          quote: data.quote,
+          character: data.character,
+          isLoaded: true
+        });
+      });
+  };
 
   render() {
-    return this.state.isLoaded ? (
+    const { isLoaded, quote, character } = this.state;
+
+    return isLoaded ? (
       <div className="App">
-          <div id="quote-container">
-            <blockquote className="blockquote text-right">
-              <p className="mb-0">{this.state.quote}</p>
-              <footer className="blockquote-footer">{this.state.character}</footer>
-            </blockquote>
-            <div id="buttons">
-            <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text="${this.state.quote}"   ${this.state.character}`}
-            target="_blank"><i className="fab fa-twitter fa-2x"></i></a>
-            <button id="new-quote" type="button" onClick={this.fetchData}>New Quote</button>
-            </div>
+        <div id="quote-container">
+          <blockquote className="blockquote text-right">
+            <p className="mb-0">{quote}</p>
+            <footer className="blockquote-footer">{character}</footer>
+          </blockquote>
+          <div id="buttons">
+            <a
+              id="tweet-quote"
+              href={`https://twitter.com/intent/tweet?text="${
+                this.state.quote
+              }"   ${character}`}
+              target="_blank"
+            >
+              <i className="fab fa-twitter fa-2x" />
+            </a>
+            <button id="new-quote" type="button" onClick={this.fetchData}>
+              New Quote
+            </button>
           </div>
+        </div>
       </div>
     ) : (
       <div className="App">
-        <div className="loader"></div>
+        <div className="loader" />
       </div>
-    )
+    );
   }
 }
 
